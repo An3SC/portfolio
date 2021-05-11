5421ab5b7e3d3045745395904e2c305d632a8e20
An3SC <asierracardalda@outlook.es>
2021-05-11 17:19:08 +0200

    created contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import clipboard from '../icons/clipboard';
+import clipboard2 from '../icons/clipboard2';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const Contact = () => {
+    const [copied, setCopied] = useState(false);
     return (
-        <div className="flex flex-col items-center my-5">
+        <div className="contact flex flex-col items-center my-5">
             <div className="container bg-2-hex p-4">
-                <h1>Contact</h1>
+                <h1 className="mb-3">Contact</h1>
+                <li>
+                    <a
+                        href="https://www.linkedin.com/in/andres-sc/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="col-4-hex underline"
+                    >
+                        Linkedin
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://github.com/An3SC"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="col-4-hex underline"
+                    >
+                        Github
+                    </a>
+                </li>
+                <li>+34 671 893 602</li>
+                <div className="flex items-center">
+                    <li>sierracardalda@gmail.com</li>
+                    <CopyToClipboard
+                        onCopy={() => setCopied(!copied)}
+                        text={'sierracardalda@gmail.com'}
+                    >
+                        {copied ? (
+                            <button>{clipboard2}</button>
+                        ) : (
+                            <button>{clipboard}</button>
+                        )}
+                    </CopyToClipboard>
+                </div>
             </div>
         </div>
     );
