d6f9b1b5b97ac5a01b245d55f2d4a310f67bc21d
An3SC <asierracardalda@outlook.es>
2021-05-11 17:19:36 +0200

    created footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import github from '../icons/github';
 
 const Footer = () => {
     return (
-        <div className="container-xl mx-auto flex items-center h-24 px-4 bg-2-hex">
-            <h1>FOOTER</h1>
+        <div className="footer container-xl mx-auto px-4 bg-2-hex">
+            <div className="flex flex-col items-end">
+                <div className="flex items-center mt-5">
+                    <p>Made with React and Tailwind</p>
+                    <a
+                        href="https://github.com/An3SC/portfolio"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        {github}
+                    </a>
+                </div>
+                <p>
+                    Brand icons made by
+                    <a
+                        href="https://www.coradvil.com/"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="col-4-hex ml-2 underline"
+                    >
+                        Cora D.
+                    </a>
+                </p>
+            </div>
         </div>
     );
 };
